f9c3a2d3fa9e1c2c5111187ee10fc4926a45ba35
YanaiAU <157066993+YanaiAU@users.noreply.github.com>
2025-11-04 06:07:46 +0200

    Update code_1.cs

--- code_1.cs
+++ code_1.cs
@@ -1,1 +1,51 @@
+using System;
+using System.Collections.Generic;
 
+class Player
+{
+    static void Main(string[] args)
+    {
+        int width = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+
+        string[] grid = new string[height];
+        for (int y = 0; y < height; y++)
+        {
+            grid[y] = Console.ReadLine();
+        }
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[y][x] == '0')
+                {
+                    int rightX = -1, rightY = -1;
+                    int bottomX = -1, bottomY = -1;
+
+                    for (int nx = x + 1; nx < width; nx++)
+                    {
+                        if (grid[y][nx] == '0')
+                        {
+                            rightX = nx;
+                            rightY = y;
+                            break;
+                        }
+                    }
+
+                    for (int ny = y + 1; ny < height; ny++)
+                    {
+                        if (grid[ny][x] == '0')
+                        {
+                            bottomX = x;
+                            bottomY = ny;
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine($"{x} {y} {rightX} {rightY} {bottomX} {bottomY}");
+                }
+            }
+        }
+    }
+}
